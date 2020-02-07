59d249f5c049333d0ec721fafc890828dc5df6bc
NBarkouky <noha.el.barkouky@gmail.com>
2020-02-07 15:30:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def home():
 def predict():
     int_features = [float(x) for x in request.form.values()]
     #final_features = [np.array(int_features)]
-    #prediction = model.predict(final_features)
-    prediction = model.predict(int_features)
+    prediction = model.predict(final_features)
+	#prediction = model.predict(int_features)
     print(prediction[0])
 
     #output = round(prediction[0], 2)
@@ -30,8 +30,8 @@ def predict_api():
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
+	print(data) 
     prediction = model.predict([np.array(list(data.values()))])
-
     output = prediction[0]
     return jsonify(output)
 
